1ba03b8e6d58493b44b51cc88ee2a4bc4d284330
Claudia <claudia@Claudias-MacBook-Pro.local>
2020-05-01 23:44:58 +0200

    better description of installation

--- README.md
+++ README.md
@@ -2,10 +2,28 @@
 
 A Firefox extension to quickly count the number of SERPs with(out) entity cards. 
 
-Implemented for Google and DuckDuckGo.
+Implemented for Google, Bing and DuckDuckGo.
 
-Installation instructions for temporary add-ons can be found [here](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension).
+Installation instructions for temporary add-ons are simple: clone the repository; open `about:debugging` in Firefox and click <kbd>Load Temporary Add-on</kbd>. Navigate to the `manifest.json` file and you are set.
 
-Once the temporary add-on is installed, use Google/DuckDuckGo as usual. When the extension is triggered a small border appears around the SERP: `peachPuff` if an entity card was detected, `cornflowerBlue` otherwise. The web console shows off the total counts. The data is stored in local storage, **and is lost when the browser is restarted**.
+Once the temporary add-on is installed, use Google/DuckDuckGo/Bing as usual. When the extension is triggered a small border appears around the SERP: `peachPuff` if one or more entity cards were detected, `cornflowerBlue` otherwise. The web console shows off the total counts. The data is stored in local storage, **and is lost when the browser is closed/restarted**.
 
 *That doesn't seem very useful?* Right! Put together just to get an idea of how many SERPs contain an entity card during regular search sessions.
+
+## Detection of Entity Cards
+
+The detection of entity cards is brittle: it depends on the detection of a particular class name:
+
+| Search Engine     | class |
+|------------|-------------------|
+| Google     | `knowledge-panel` |
+| DuckDuckGo | `module--about`   |
+| Bing       | `b_entityTitle`   |
+
+*This might change at any time!*
+
+## Bing Issues
+
+Depending on from where you access [bing.com](https://www.bing.com) entity cards might never appear. Concretely, check out the difference between `bing.nl` and `bing.co.uk` for the query `Barack Obama` (or any other query with an entity in it).
+
+While DDG and Google have one entity card visible on the SERP, Bing at times has several (how many depends on your definition of an entity card). Independent of the number of entity cards (beyond 1), this results in just one increment in the entity counter.
--- counter.js
+++ counter.js
@@ -1,27 +1,33 @@
+//@ts-check
+
+const STYLE = "5px solid "; 
+const ENTCOLOR = "peachPuff";
+const NOENTCOLOR = "cornflowerBlue";
+
 /*
  * 1) retrieves the current SERP and Entity counters from local storage;
  * 2) sets the starting date for the counters if necessary
  */
-function getItemCounts(item){
+function getItemCounts(item) {
     let serp = localStorage.getItem(item.serp);
     let entity = localStorage.getItem(item.entity);
     let startingDate = localStorage.getItem(item.startingDate);
-    if(serp == null){
-        serp = 0;
+    if (serp == null) {
+        serp = "0"; //keeping it consistent with type of local storage
     }
-    if(entity == null){
-        entity = 0;
+    if (entity == null) {
+        entity = "0";
     }
-    if(startingDate == null){
-        localStorage.setItem(item.startingDate, new Date());
+    if (startingDate == null) {
+        localStorage.setItem(item.startingDate, new Date().toString());
     }
-    return [serp, entity];
+    return [Number.parseInt(serp), Number.parseInt(entity)];
 }
 
 /*
  * Writes the serp / entity counts to local storage.
  */
-function setItemCounts(item, serp, entity){
+function setItemCounts(item, serp, entity) {
     localStorage.setItem(item.serp, serp);
     localStorage.setItem(item.entity, entity);
 }
@@ -34,60 +40,65 @@ function setItemCounts(item, serp, entity){
  * 5) Write updated counters to local storage.
  * 6) Write info on the console.
  */
-function update(item){
+function update(item) {
 
     let [serp, entity] = getItemCounts(item);
     serp++; //+1 because the script is triggered when a SERP is observed
-    
+
     //entity card present?
-    console.log("Checking for entity card with className: "+item.className);
-    console.log(document.getElementsByClassName(item.className).length);
-    if(document.getElementsByClassName(item.className).length>0){
+    let whichColor = NOENTCOLOR;
+    if (document.getElementsByClassName(item.className).length > 0) {
         entity++;
-        document.body.style.border = "5px solid "+item.entityColor;
+        whichColor = ENTCOLOR;
     }
-    else {
-        document.body.style.border = "5px solid "+item.serpColor;
-    }
-    
+    document.body.style.border = STYLE + whichColor;
+
     //write updated counts to local storage
     setItemCounts(item, serp, entity);
-    
-    //a bit of info on the console
+
+    //dump info on the console
     console.table({
-        "SE" : item.name,
-        "Data collected since": localStorage.getItem( item.startingDate ),
-        "SERPs" : serp,
-        "SERPs /w entities" : entity
+        "SE": item.name,
+        "Data collected since": localStorage.getItem(item.startingDate),
+        "SERPs": serp,
+        "SERPs /w entities": entity
     });
 }
 
-let google = {
-    name : "GOOGLE",
-    serp : "googleSERP",
-    entity : "googleEntity",
-    startingDate : "googleStart",
-    className : "knowledge-panel", //based on inspection of Google SERP (might change)
-    entityColor : "peachPuff", //page border color to show off serp + entity observed
-    serpColor : "cornflowerBlue" //page border color to show off serp but no entity observed
+let googleObject = {
+    name: "GOOGLE",
+    serp: "googleSERP",
+    entity: "googleEntity",
+    startingDate: "googleStart",
+    className: "knowledge-panel" //based on inspection of SERP (might change)
 }
 
-let duckduckgo = {
-    name : "DUCKDUCKGO",
-    serp : "ddgoSERP",
-    entity : "ddgoEntity",
-    startingDate : "ddgoStart",
-    className : "module--about", //based on inspection of Google SERP (might change)
-    entityColor : "peachPuff", //page border color to show off serp + entity observed
-    serpColor : "cornflowerBlue" //page border color to show off serp but no entity observed
+let ddgoObject = {
+    name: "DUCKDUCKGO",
+    serp: "ddgoSERP",
+    entity: "ddgoEntity",
+    startingDate: "ddgoStart",
+    className: "module--about"
 }
 
-let loc = window.location.href.toString();
-
-if(loc.includes("google")){
-    window.addEventListener("load", function(){ update(google) }, false);
+let bingObject = {
+    name: "BING",
+    serp: "bingSERP",
+    entity: "bingEntity",
+    startingDate: "bingStart",
+    className: "b_entityTitle"
 }
 
-if(loc.includes("duckduckgo")){
-    window.addEventListener("load", function(){ update(duckduckgo) }, false);
+let loc = window.location.hostname.toString();
+
+if (loc.includes("google")) {
+    update(googleObject);
+}
+
+if (loc.includes("duckduckgo")) {
+    update(ddgoObject);
+}
+
+if (loc.includes("bing")) {
+    update(bingObject);
 }
